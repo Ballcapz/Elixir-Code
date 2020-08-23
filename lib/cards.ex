82926c0969e36afbfc0935cc56fd9c0fce31f987
Zach Johnson <zlg23johnson@gmail.com>
2020-08-22 21:24:37 -0500

    Finished section 2 of udemy course

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,4 @@
 defmodule Cards do
-
-
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -22,5 +20,24 @@ defmodule Cards do
     Enum.split(deck, hand_size)
   end
 
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} ->
+        :erlang.binary_to_term(binary)
+      _ ->
+        "Error loading file"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck()
+    |> Cards.shuffle()
+    |> Cards.deal(hand_size)
+  end
 
 end
